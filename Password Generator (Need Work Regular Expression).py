cc2024f8e13a33e79d27e2630459b47686d18a21
Daniel Kim <danielkim848@gmail.com>
2024-02-28 16:27:26 -0800

    Update Password Generator (Need Work Regular Expression).py

--- Password Generator (Need Work Regular Expression).py
+++ Password Generator (Need Work Regular Expression).py
@@ -3,3 +3,7 @@ A regular expression, or regex, is a pattern used to match a specific combinatio
 The compile() function from the re module compiles the string passed as the argument into a regular expression object that can be used by other re methods.
 
 Character classes also allow you to indicate a range of characters to match. You need to specify the starting and the ending characters separated by an hyphen, -. Characters follow the Unicode order.
+
+Since the underscore character is a valid character for variable names, it is included in the \w character class (equivalent to [a-zA-Z0-9_]), which can be conveniently used to match variable names.
+
+Therefore, the \W character class is equivalent to [^a-zA-Z0-9_] with the underscore character that is not matched. For this reason you cannot use it to match all your special characters.
